ba2554b3aceef4a8b2d5e6d5548b3a27a0ace1ea
Genteroyezeck <ezeckieljamesgenteroy@gmail.com>
2024-05-20 13:11:57 +0800

    Update connection.php

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+$host = 'localhost';
+$dbname = 'rental_system';
+$username = 'ezeck';
+$password = 'ezeck';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // Set PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    // If connection fails, display error message
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
